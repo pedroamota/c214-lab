94c74bb272e90566e25b01fe7e462b2e41450f5f
Pedro Mota <pedrodaluzmota208@gmail.com>
2024-03-16 21:38:00 -0300

    chore: Add test todo

---
+++ src/todo.test.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { TodoList } from "./ToDoList"
+
+const anyTask = {
+  title: 'any_title',
+  description: 'any_description',
+  targetDate: '01/01/2025',
+  type: 'any_type',
+  priority: '1',
+  subTasks: []
+}
+
+describe('ToDoList', () => {
+  describe('Testing add', () => {
+    test('should add a new task to the list', () => {
+      const todoInstance = new TodoList()
+      todoInstance.add(anyTask)
+      const tasks = todoInstance.getTasks()
+      expect(tasks).toEqual([anyTask])
+    })
+
+    test('should add a valid tasks', () => {
+      const todoInstance = new TodoList()
+      const invalidValue: any = {
+        invalidField: 'invalidValue'
+      }
+      todoInstance.add(invalidValue)
+      const tasks = todoInstance.getTasks()
+      expect(tasks).toEqual([])
+    })
+  })
+})
