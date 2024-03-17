00544f03f8fb5c5deb84346fd388f445fe871fc0
Pedro Mota <pedrodaluzmota208@gmail.com>
2024-03-16 22:27:48 -0300

    feat: Novas switches de testes (update / delete)

--- src/todo.test.ts
+++ src/todo.test.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { ToDoList } from "./ToDoList"
+import { ToDoList, UpdateTask } from "./ToDoList"
 
 const anyTask = {
   title: 'any_title',
@@ -29,4 +29,33 @@ describe('ToDoList', () => {
       expect(tasks).toEqual([])
     })
   })
+  
+  describe('Testing update', () => {
+    test('Update test', () => {
+      const todoInstance = new ToDoList()
+      todoInstance.add(anyTask)
+
+      const updatedTask: UpdateTask = {
+        title: "UpdatedTask"
+      }
+
+      todoInstance.updateTask(0,updatedTask)
+      const tasks = todoInstance.getTasks()
+
+      expect(tasks[0].title).toEqual(updatedTask.title)
+    })
+  })
+
+  describe('Testing delete', () => {
+    test('Delete test', () => {
+      const todoInstance = new ToDoList()
+      todoInstance.add(anyTask)
+
+      todoInstance.removeTask(0)
+      const tasks = todoInstance.getTasks()
+
+      expect(tasks).toEqual([]);
+    })
+  })
+
 })
