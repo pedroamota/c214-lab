1640d067d9ac84a26a4ffbb0b6a8bc77b0473fc1
Pedro Mota <pedrodaluzmota208@gmail.com>
2024-04-04 10:28:43 -0300

    :test_tube: Test: Importando Moeda no const

--- src/test/MoedasConst.ts
+++ src/test/MoedasConst.ts
@@ -1,7 +1,9 @@
+import { Moeda } from '../main/Moeda.js';
+
 class MoedasConst {
 
-  static readonly REAL: number = 4.57;
-  static readonly BITCOIN: number = 0.000015;
-  static readonly DOLLAR: number = 1;
+  static readonly REAL: Moeda = new Moeda('Real', 4.57);
+  static readonly BITCOIN: Moeda = new Moeda('Bitcoin', 0.000015);
+  static readonly DOLAR: Moeda = new Moeda('Dolar', 1);
 
 }
