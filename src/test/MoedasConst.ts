7f096f4ff123607e8b277a3955012074f1928796
Pedro Mota <pedrodaluzmota208@gmail.com>
2024-04-04 11:23:30 -0300

    :test_tube: Test: Atualizando valor do real

--- src/test/MoedasConst.ts
+++ src/test/MoedasConst.ts
@@ -2,7 +2,7 @@ import { Moeda } from '../main/Moeda.js';
 
 class MoedasConst {
 
-  static readonly REAL: Moeda = new Moeda('Real', 4.57);
+  static readonly REAL: Moeda = new Moeda('Real', 5.02);
   static readonly BITCOIN: Moeda = new Moeda('Bitcoin', 0.000015);
   static readonly DOLAR: Moeda = new Moeda('Dolar', 1);
 
